f03c822fafb151ca43f339168cade32fc82c4c92
Aditya Singh Tomar <adityasinght970@gmail.com>
2026-09-07 21:19:15 +0530

    feat(terminal-todo): add save method

--- storage.go
+++ storage.go
@@ -1,9 +1,27 @@
 package main
 
-type Storage[T any] struct{
+import (
+	"encoding/json"
+	"os"
+)
+
+type Storage[T any] struct {
 	FileName string
 }
 
-func NewStorage[T any](fileName string) *Storage[T]{
+func NewStorage[T any](fileName string) *Storage[T] {
 	return &Storage[T]{FileName: fileName}
 }
+
+// save method
+func (s *Storage[T]) save(data T) error {
+	fileData, err := json.MarshalIndent(data, "", "    ")
+
+	if err != nil{
+		return nil
+	}
+
+	return os.WriteFile(s.FileName, fileData, 0644)
+
+}
+
